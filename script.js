aaef450240ccbb8c09abdbe7f4bbd2753175f371
Kanak Agrawal <akanak101@gmail.com>
2021-10-15 12:35:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+replaceText(document.body)
+
+function replaceText(element) 
+{
+    if (element.hasChildNodes()) 
+    {
+        element.childNodes.forEach(replaceText)
+    }
+    else if (element.nodeType === Text.TEXT_NODE)
+    {
+        if (element.textContent.match(/coronavirus/gi))
+        {
+            const newElement = document.createElement('span')
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,
+            '<span style= "background-color: black; color:black;">$1</span>')
+            element.replacewith(newElement)
+        }
+
+       /* if (element.textContent.match(/coronavirus/gi))
+        {
+            element.parentElement.style.color = 'black'
+            element.parentElement.style.backgroundColor = 'black'
+        } */
+       // element.textContent = element.textContent.replace(/coronavirus/gi, 'OK')
+    }
+}
